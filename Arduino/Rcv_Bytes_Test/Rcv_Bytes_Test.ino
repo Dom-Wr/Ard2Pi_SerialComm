6f12b75dec24b07e224fdb54a4572cc220a2380d
Dom-Wr <dominiquewright250@gmail.com>
2017-02-03 12:59:59 -0600

    Cmnds Dev Branch

---
+++ Arduino/Rcv_Bytes_Test/Rcv_Bytes_Test.ino
@@ -0,0 +1,35 @@
+#include <Message_Struct.h>
+
+Message_Struct message;
+unsigned char bytesarray[4] = {182,243,157,63};
+unsigned char byte_rcv[4];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //Serial.println("The data transmission has started");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(bytesarray,4);
+  if (Serial.available() > 0)
+  {
+    Serial.print("Serial is available");
+    Serial.readBytes(byte_rcv,Serial.available());
+  }
+  
+  for (int i=0; i<4; i++)
+  {
+    Serial.println();
+    Serial.print("The byte in position ");
+    Serial.print(i+1);
+    Serial.print(" is:");
+    Serial.print(byte_rcv[i]);
+    Serial.println();
+  }
+
+  delay(1000);
+
+
+}
---
+++ Linux/Comm_Gui/Snd_Cmds_Test.py
@@ -0,0 +1,53 @@
+from WheelBotMsg import WheelBotMsg
+from cmd import Cmd
+import time
+
+
+Message = WheelBotMsg('../../../../../dev/ttyACM0')
+
+class MyPrompt(Cmd):
+
+    def do_start_comm(self, args):
+        """Starts Comm System on a different Thread and initiates Arduino"""
+        Message.run_comm = True
+        Message.RunComm()
+
+    def do_stop_comm(self, args):
+        """Stops Arduino Communication and Data Logging and Exits Command Prompt"""
+        Message.run_comm = False
+        Message.data_logging = False
+        raise SystemExit
+
+    def do_heading_print(self,args):
+        """Prints the current heading of the Rover"""
+        print Message.WheelBotTlm.heading[0]
+
+    def do_send_cmd(self,args):
+        """Sends current commands to the Arduino"""
+        Message.send()
+        print "The commands have been sent to the Arduino"
+
+
+    def do_log_data(self, args):
+        """Begins logging data to csv file"""
+        print "The lograte is: "
+        Message.data_logging = True
+
+    def do_stop_log_data(self, args):
+        """Ends loggging data"""
+        Message.data_logging = False
+
+#    def do_set_lograte(self, args):
+#        """"Allows user to set the frequency data will be logged to a csv"""
+#        Message.log_frequency = args
+#        print Message.log_frequency
+
+    def do_q(self, args):
+        """Force quits the program"""
+        print "Force Quit"
+        raise SystemExit
+
+if __name__ == '__main__':
+    prompt = MyPrompt()
+    prompt.prompt = '>'
+    prompt.cmdloop('Ard2Pi Comm System Prompt...')
